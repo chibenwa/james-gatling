5ac60f1f33b9199a9484a09dd390c1bc8ef3fa71
Benoit Tellier <btellier@linagora.com>
2020-06-12 11:03:31 +0700

    pop

--- src/test/scala-2.12/org/apache/james/gatling/simulation/smtp/SmtpNoAuthenticationNoEncryptionBigBodySimulation.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/smtp/SmtpNoAuthenticationNoEncryptionBigBodySimulation.scala
@@ -1,9 +1,18 @@
 package org.apache.james.gatling.simulation.smtp
 
 import io.gatling.core.Predef._
-import org.apache.james.gatling.simulation.{Configuration, SimulationOnMailCorpus}
+import io.gatling.core.structure.ScenarioBuilder
+import org.apache.james.gatling.simulation.{Configuration, HttpSettings, SimulationOnMailCorpus, UsersTotal}
 import org.apache.james.gatling.smtp.scenari.SmtpNoAuthenticationNoEncryptionBigBodyScenario
 
 class SmtpNoAuthenticationNoEncryptionBigBodySimulation extends Simulation with SimulationOnMailCorpus {
-  setUp(injectUsersInScenario(new SmtpNoAuthenticationNoEncryptionBigBodyScenario().generate(Configuration.ScenarioDuration, feeder)))
+  setUp(
+    injectUsers(new SmtpNoAuthenticationNoEncryptionBigBodyScenario()
+      .generate(Configuration.ScenarioDuration, feeder)))
+
+  def injectUsers(scenario: ScenarioBuilder) = {
+    scenario
+      .inject(UsersTotal(Configuration.UserCount).injectDuring(Configuration.InjectionDuration))
+      .protocols(HttpSettings.httpProtocol)
+  }
 }
