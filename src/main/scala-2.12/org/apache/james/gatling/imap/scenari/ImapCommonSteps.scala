160627bbcc5e9c058e7fa1babfde29d5929e4a40
Benoit Tellier <btellier@linagora.com>
2021-03-03 16:26:53 +0700

    BIG APPENDS

--- src/main/scala-2.12/org/apache/james/gatling/imap/scenari/ImapCommonSteps.scala
+++ src/main/scala-2.12/org/apache/james/gatling/imap/scenari/ImapCommonSteps.scala
@@ -8,14 +8,20 @@ import com.linagora.gatling.imap.protocol.command.MessageRange.Last
 import com.linagora.gatling.imap.protocol.command.MessageRanges
 import io.gatling.core.Predef._
 
+import scala.util.Random
+
 object ImapCommonSteps {
+  private val myRandom = Random.alphanumeric
+  private def generateMessage() : String =
+    myRandom.grouped(200).flatMap(_.append(Stream('\r', '\n'))).take(1024 * 1024).mkString
+
   val receiveEmail = exec(imap("append").append("INBOX", Some(scala.collection.immutable.Seq("\\Flagged")), Option.empty[Calendar],
-    """From: expeditor@example.com
+    s"""From: expeditor@example.com
       |To: recipient@example.com
       |Subject: test subject
       |
       |Test content
-      |abcdefghijklmnopqrstuvwxyz
+      |${generateMessage()}
       |0123456789""".stripMargin).check(ok))
 
   val readLastEmail = exec(imap("list").list("", "*").check(ok, hasFolder("INBOX")))
